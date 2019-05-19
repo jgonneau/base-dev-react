8e16176f8cac65f484905c617b78e96e2619a15f
jgonneau <jgonneau@github.com>
2019-05-20 00:07:38 +0200

    Init.

---
+++ README.md
@@ -0,0 +1,5 @@
+## BASE DEV REACT
+
+Boilerplate for React Development with Node as Back-end && Mongodb as Database
+
+# by jgonneau
---
+++ back/server.js
@@ -0,0 +1,25 @@
+const	express			= require('express'),
+		app				= express(),
+		session			= require('express-session'),
+		MongoStore		= require('connect-mongo')(session),
+		path			= require('path'),
+		engine			= require('ejs-mate'),
+		cookieParser	= require('cookie-parser'),
+		bodyParser		= require('body-parser'),
+		server			= require('http').createServer(app),
+		s3cr3t			= "0987654321",
+        lport = process.env.PORT || 3000;
+
+//Creation de routes
+app.get('/', function(req, res) {
+    res.send('home');
+})
+.use(function(req, res, next) {
+    res.send('<p>404</p>');
+});
+
+
+//Demarrage de serveur:
+server.listen(lport, function() {
+    console.log('Server starting... on '+lport+'.\n');
+});
